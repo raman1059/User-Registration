0431dd27cb91d19705407efad3e2e2103c48bc72
Saalim <mohammad.saalim@bridgingtech.com>
2019-06-17 16:13:33 +0530

    Routes Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,34 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { Routes, RouterModule } from "@angular/router";
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DataFormComponent
+  },
+  {
+    path: 'user/:uid',
+    component: DisplayUserDataComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DataFormComponent,
+    DisplayUserDataComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

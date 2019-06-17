2f1a9f0829515ad9966a1929ed065f4b51b7bb49
Saalim <mohammad.saalim@bridgingtech.com>
2019-06-17 03:12:22 -0700

    initial commit

---
+++ src/app/data-form/data-form.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'data-form',
+  templateUrl: './data-form.component.html',
+  styleUrls: ['./data-form.component.css']
+})
+export class DataFormComponent implements OnInit {
+  registered = false;
+	submitted = false;
+	userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  invalidFirstName()
+  {
+  	return (this.submitted && this.userForm.controls.first_name.errors != null);
+  }
+
+  invalidLastName()
+  {
+  	return (this.submitted && this.userForm.controls.last_name.errors != null);
+  }
+
+  invalidEmail()
+  {
+  	return (this.submitted && this.userForm.controls.email.errors != null);
+  }
+
+  invalidZipcode()
+  {
+  	return (this.submitted && this.userForm.controls.zipcode.errors != null);
+  }
+
+  invalidPassword()
+  {
+  	return (this.submitted && this.userForm.controls.password.errors != null);
+  }
+
+  ngOnInit() {
+
+  	this.userForm = this.formBuilder.group({
+  		first_name: ['', Validators.required],
+  		last_name: ['', Validators.required],
+  		email: ['', [Validators.required, Validators.email]],
+  		zipcode: ['', [Validators.required, Validators.pattern('^[0-9]{6}(?:-[0-9]{4})?$')]],
+  		password: ['', [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+  	});
+  }
+  onSubmit()
+  {
+  	this.submitted = true;
+
+  	if(this.userForm.invalid == true)
+  	{
+  		return;
+  	}
+  	else
+  	{
+  		this.registered = true;
+  	}
+  }
+}
